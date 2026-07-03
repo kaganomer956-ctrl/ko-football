c677c1fc2c6d24f88ef2332e4d538c8c7504929f
kaganomer956-ctrl <kaganomer956@gmail.com>
2026-07-03 10:59:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -213,3 +213,28 @@ counter.innerText=target;
 update();
 
 });
+/*=========================
+SMOOTH SCROLL
+=========================*/
+
+document.querySelectorAll('a[href^="#"]').forEach(link=>{
+
+link.addEventListener("click",function(e){
+
+e.preventDefault();
+
+const target=document.querySelector(this.getAttribute("href"));
+
+if(target){
+
+target.scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+}
+
+});
+
+});
